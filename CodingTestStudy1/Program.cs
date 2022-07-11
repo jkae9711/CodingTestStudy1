89d45c31b015bddd8df3f79d8ecaabdb1574596a
HSH <jkae9711@naver.com>
2022-07-11 11:56:07 +0900

    Week2

--- CodingTestStudy1/Program.cs
+++ CodingTestStudy1/Program.cs
@@ -22,6 +22,15 @@ namespace CodingTestStudy1
             //Console.ReadLine();
 
             #endregion
+
+            #region Week2
+
+            Week2 test = new Week2();
+
+            Console.WriteLine(test.solution(new int[] { 95, 90, 99, 99, 80, 99 }, new int[] { 1, 1, 1, 1, 1, 1 }));
+            Console.ReadLine();
+
+            #endregion
         }
     }
 }
