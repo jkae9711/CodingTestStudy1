b4cf6254d5f397921864e71ad0a7d76aa29548a5
SangHoon Hwang <jkae9711@naver.com>
2022-08-19 13:21:20 +0900

    Week7_1 주석 추가

--- CodingTestStudy1/Level2/Week7_1.cs
+++ CodingTestStudy1/Level2/Week7_1.cs
@@ -1,5 +1,6 @@
 ﻿// #Week7_1 : H-Index
 
+using System;
 using System.Linq;
 
 public class Week7_1
@@ -7,6 +8,7 @@ public class Week7_1
     public int solution(int[] citations)
     {
         int[] intArrHValue = new int[citations.Length];
+        int iAnswer = 0;
 
         for (int iIndex1 = 0; iIndex1 < citations.Length; iIndex1++)
         {
@@ -14,21 +16,20 @@ public class Week7_1
 
             for (int iIndex2 = 0; iIndex2 < citations.Length; iIndex2++)
             {
+                if (citations[iIndex1] == 0) // h가 0인 경우에는 탐색 안함
+                    break;
+
                 if (citations[iIndex1] <= citations[iIndex2]) // h번 이상 인용된 논문의 개수 찾기
                     iHitCount++;
 
-                if (iHitCount == citations[iIndex1]) // h번 이상 인용된 논문의 개수가 h번이 되면 그 이상 찾을 필요 없으므로 break;
+                if (iHitCount == citations[iIndex1]) // h번 이상 인용된 논문의 개수가 h개가 되면 그 이상 찾을 필요 없으므로 break;
                     break;
             }
 
-            if (iHitCount <= citations[iIndex1])
-                intArrHValue[iIndex1] = iHitCount;
-            else
-                intArrHValue[iIndex1] = 0;
-
-            iHitCount = 0;
+            if (iHitCount >= iAnswer) // h의 최대값 갱신
+                iAnswer = iHitCount;
         }
 
-        return intArrHValue.Max();
+        return iAnswer;
     }
 }
