fce581d5e0a567134eb65b1ebcaf69818857056e
SangHoon Hwang <jkae9711@naver.com>
2022-08-25 14:06:06 +0900

    Week8_2

--- CodingTestStudy1/Level2/Week8_2.cs
+++ CodingTestStudy1/Level2/Week8_2.cs
@@ -6,7 +6,9 @@ public class Week8_2
 {
     public int[] solution(int brown, int yellow)
     {
-        int height = 0, width = 0;
+        int width = 0, height = 0;
+        // 리턴하고자 하는 가로, 세로 값 선언
+
         List<int[]> lstXY = new List<int[]>();
 
         for (int i = 1; i <= yellow; i++)
@@ -26,13 +28,13 @@ public class Week8_2
         {
             if (brown == (item[0] + 2) * 2 + item[1] * 2)
             {
-                height = item[0] + 2;
-                width = item[1] + 2;
+                width = item[0] + 2;
+                height = item[1] + 2;
 
                 break;
             }
         }
 
-        return new int[] { height, width };
+        return new int[] { width, height };
     }
 }
